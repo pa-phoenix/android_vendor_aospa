pa-phoenix/android_vendor_pa

products: Add REDMI NOTE 10 PRO/MAX (sweet)
Signed-off-by: Rasenkai <rasenkai99@gmail.com>

products/AndroidProducts.mk
@@ -43,6 +43,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/phoenix/pa_phoenix.mk \
     $(LOCAL_DIR)/raphael/pa_raphael.mk \
     $(LOCAL_DIR)/sagit/pa_sagit.mk \
+    $(LOCAL_DIR)/sweet/pa_sweet.mk \
     $(LOCAL_DIR)/wayne/pa_wayne.mk \
     $(LOCAL_DIR)/X00TD/pa_X00TD.mk
 
@@ -75,5 +76,6 @@ COMMON_LUNCH_CHOICES += \
     pa_phoenix-userdebug \
     pa_raphael-userdebug \
     pa_sagit-userdebug \
+    pa_sweet-userdebug \
     pa_wayne-userdebug \
     pa_X00TD-userdebug

pa-phoenix/android_vendor_aospa

Change-Id: I5defad33c74be2fd3d812ee289cd379b26de8984

products/AndroidProducts.mk
@@ -16,10 +16,12 @@
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
     $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk \
-    $(LOCAL_DIR)/phoenix/aospa_phoenix.mk
+    $(LOCAL_DIR)/phoenix/aospa_phoenix.mk \
+    $(LOCAL_DIR)/sweet/aospa_sweet.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
     aospa_oneplus9-userdebug \
     aospa_oneplus9pro-userdebug \
-    aospa_phoenix-userdebug
+    aospa_phoenix-userdebug \
+    aospa_sweet-userdebug